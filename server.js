c4a10c33e378e2ee12c4df63d16cd9a81fc797da
ConnieP01 <57119043+ConnieP01@users.noreply.github.com>
2023-03-01 22:16:06 -0800

    image update

--- server.js
+++ server.js
@@ -25,10 +25,7 @@ const loginHandler = require('./handlers/login.js')
 const listItemsHandler = require('./handlers/listItems.js')
 const registerHandler = require('./handlers/register.js') 
 const createItemHandler = require('./handlers/createItems.js') 
-<<<<<<< Updated upstream
 //const createItemHandler = require('./handlers/createItem.js')
-=======
->>>>>>> Stashed changes
 const profileHandler = require('./handlers/profile.js')
 //const overviewHandler = require('./handlers/overview.js')
 

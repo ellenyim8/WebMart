2a9b91a1a85b573b6fc5616978f5adcf163457c6
sweucr <69826832+ellenyim8@users.noreply.github.com>
2023-03-01 19:48:48 -0800

    stashing----ignore

--- handlers/login.js
+++ handlers/login.js
@@ -1,10 +1,12 @@
+const { getRegister } = require("./register");
+
 function getLogin(request, response){
   if(request.session.logged_in){
     console.log("Redirecting to home, current user: "+request.session.username);
     response.redirect("/home");
   }
   else{
-    response.render('login', {title: 'Auction - Login'});
+    response.render('login', {title: 'WebMart - Login'});
   }
 }
 
